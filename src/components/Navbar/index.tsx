62dd5ec9eb40a751ff11b55a406d002804a8bc44
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2026-04-28 16:53:28 -0300

    primer commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import Logo from "../../assets/crimsonlogo.png";
+
+const Navbar  = () => {
+  return (
+    <nav className="bg-black text-white  border-b border-neutral-800">
+        <div className="flex justify-between items-center max-w-6xl mx-auto w-full px-4">
+            <img src={Logo} alt="Crimson Bar logo" className="h-30 object-contain cursor-pointer" />
+
+            <ul className="flex gap-3 cursor-pointer">
+                <li className="hover:text-red-600 transition-colors">Home</li>
+                <li className="hover:text-red-600 transition-colors">Cocktails</li>
+                <li className="hover:text-red-600 transition-colors">Categorías</li>
+                <li className="hover:text-red-600 transition-colors">Favoritos</li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar 

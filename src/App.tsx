3735ffec7016688a7080da99846d7592dfcf754f
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2026-05-05 17:55:00 -0300

    Arreglando pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/CrimsonBar">
       <Routes>
         <Route element={<Layout/>}>
           <Route path="/" element={<Home/>}/>
